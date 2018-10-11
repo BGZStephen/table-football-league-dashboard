fbd1eb7d8f9674eb1814a1894b284872183c9c77
stephen <sjw948@gmail.com>
2018-10-11 20:23:08 +0100

    player add form + validation

--- src/components/dashboard/players/add.jsx
+++ src/components/dashboard/players/add.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 import Breadcrumbs from '../breadcrumbs/breadcrumbs'
+import FormError from '../../form/form-error';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class PlayersAdd extends Component {
   constructor({props}) {
     super(props)
+
+    this.state = {
+      formErrors: {name: null},
+      defender: false,
+      striker: false,
+      name: '',
+    }
   }
 
   render() {
@@ -25,10 +34,96 @@ class PlayersAdd extends Component {
       <div className="players-add full-width-container">
         <Breadcrumbs breadcrumbs={breadcrumbs} />
         <div className="content-container container-grey">
-
+          <div className="row">
+            <div className="col col-sm-3">
+              <div className="panel panel-white">
+                <p className="panel-title">Add a player</p>
+                <form>
+                  <div className="input-group">
+                    <label>Name</label>
+                    <input type="text" id="name" name="name" placeholder="Jon Doe" onChange={this.handleFormInputChange} />
+                    {this.state.formErrors.name ? <FormError message={this.state.formErrors.name} /> : null}
+                  </div>
+                  <div className="input-group">
+                    <label>Position</label>
+                    {this.state.formErrors.position ? <FormError message={this.state.formErrors.position} /> : null}
+                  </div>
+                  <div className="positions-container">
+                    <div className={this.state.striker ? "position striker active" : "position striker"} onClick={this.toggleStriker}>
+                      <FontAwesomeIcon icon="crosshairs" />
+                      <p>Striker</p>
+                    </div>
+                    <div className={this.state.defender ? "position defender active" : "position defender"} onClick={this.toggleDefender}>
+                      <FontAwesomeIcon icon="shield-alt" />
+                      <p>Defender</p>
+                    </div>
+                  </div>
+                  <div className="input-group">
+                    <button type="button" onClick={this.onSubmit}>Submit</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
+  }
+
+  toggleStriker = () => {
+    const newState = this.state;
+    newState.formErrors.position = null;
+    newState.striker = !this.state.striker;
+    this.setState(newState)
+  }
+
+  toggleDefender = () => {
+    const newState = this.state;
+    newState.formErrors.position = null;
+    newState.defender = !this.state.defender;
+    this.setState(newState)
+  }
+
+  handleFormInputChange = (event) => {
+    const id = event.target.id;
+    const value = event.target.value;
+    const newState = this.state;
+
+    newState.formErrors[id] = null;
+    newState[id] = value;
+    this.setState({state: newState})
+  }
+
+  onSubmit = () => {
+    if (!this.isFormValid()) {
+      return;
+    }
+  }
+
+  isFormValid = () => {
+    const newState = this.state;
+    let stateSetFlag = false;
+
+    for (const key of Object.keys(newState.formErrors)) {
+      newState.formErrors[key] = null;
+    }
+
+    if (!this.state.name) {
+      newState.formErrors.name = 'Name is required';
+      stateSetFlag = true;
+    }
+
+    if (!this.state.striker && !this.state.defender) {
+      newState.formErrors.position = 'Please select one or more positions';
+      stateSetFlag = true;
+    }
+
+    if (stateSetFlag) {
+      this.setState({state: newState})
+      return false;
+    }
+
+    return true;
   }
 }
 
--- src/utils/font-awesome.js
+++ src/utils/font-awesome.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faLock, faEnvelope, faBars, faGamepad, faTrophy, faUsers, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faUser, faLock, faEnvelope, faBars, faGamepad, faTrophy, faUsers, faHome, faCrosshairs, faShieldAlt } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUser, faLock, faEnvelope, faBars, faUsers, faGamepad, faTrophy, faHome)
+library.add(faUser, faLock, faEnvelope, faBars, faUsers, faGamepad, faTrophy, faHome, faCrosshairs, faShieldAlt)
